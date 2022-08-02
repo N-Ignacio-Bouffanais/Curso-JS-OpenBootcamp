31ed562c105b26280375266d46793d626d61fc82
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-08-02 16:47:58 -0400

    segundo ejercicio

---
+++ ejercicio2/ejercicio.js
@@ -0,0 +1,17 @@
+const fecha_nacimiento = new Date("august 11 1999")
+console.log(fecha_nacimiento);
+const fecha_libro = new Date(2006, 07, 17);
+console.log(fecha_libro);
+
+let YO = [
+    "Nicolas",
+    22,
+    true,
+    fecha_nacimiento,
+    libro = {
+        titulo: "El imperio final",
+        "autor": "Brandon Sanderson",
+        fecha_libro,
+        "url": "https://www.google.com/search?q=el+imperio+final+btrandon+sanderson&oq=el+imperio+final+btrandon+sanderson&aqs=chrome..69i57.5921j0j1&sourceid=chrome&ie=UTF-8"
+    },
+]

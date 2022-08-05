45c89942e1a88eb9b05662b6d4be668a9fb35ce5
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-08-05 18:36:46 -0400

    modulo 5

---
+++ modulo5/ejercicio5.js
@@ -0,0 +1,13 @@
+let alturaCM = 175
+let alturaMetros = 1.75
+let peso_kg = 61.80
+
+let altura_red = Math.ceil(alturaMetros)
+console.log(altura_red);
+
+let peso_red = Math.floor(peso_kg);
+console.log(peso_red);
+
+let valorMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(valorMax)

ad60f34b6680ae77487a7a167b51a7616e3b41d7
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-08-12 15:35:36 -0400

    ejercicio 1 modulo, set, add, delete

---
+++ Javascript-Basico/modulo7/ejercicio1.js
@@ -0,0 +1,11 @@
+
+
+const FamilyNames = new Set(["Nicolas", "Vicente", "Andrea", "Jenny"])
+
+console.log(FamilyNames)
+
+FamilyNames.add("Andrea")
+console.log(FamilyNames)
+
+FamilyNames.add("JavaScript")
+console.log(FamilyNames)

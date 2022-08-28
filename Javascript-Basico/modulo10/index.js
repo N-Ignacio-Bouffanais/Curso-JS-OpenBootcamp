e988a76f1c11e710585dc3e4cd185d427aa6bd5e
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-08-28 01:26:36 -0400

    modulo10 del bootcamp de JavaScript

---
+++ Javascript-Basico/modulo10/index.js
@@ -0,0 +1,12 @@
+import { suma, multiplica } from './controller'
+
+const sum = suma(4,5)
+console.log(sum)
+
+const multiplicacion = multiplica(1,2)
+console.log(multiplicacion)
+
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Hola, soy Nicolas un futuro desaroolador deJavaScript!'));
